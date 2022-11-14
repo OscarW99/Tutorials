b3cac715093dffdba69d00f12ba24f64767c735e
Oscar Wright <oscar-wright@mail.com>
2022-11-14 18:32:00 -0500

    Elipsis vid finished

--- Elipsis_in_R/elipsis.R
+++ Elipsis_in_R/elipsis.R
@@ -1,4 +1,26 @@
 
-
 string <- c('a', 'b', 'c', 'd', 'e')
 
+my_function <- function(vector, ...){
+
+    args <- list(...)
+
+    for (i in vector){
+        print(i)
+    }
+
+    print(args)
+}
+
+#################################################
+#################################################
+
+my_function <- function(...){
+
+    args <- list(...)
+
+    for (i in 1:length(args)){
+        arg <- args[[i]]
+        print(sqrt(arg))
+    }
+}
