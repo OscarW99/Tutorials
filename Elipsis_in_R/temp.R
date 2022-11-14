0defa4d9bb15ec06daeaa06317f68ba2a56847eb
Oscar Wright <oscar-wright@mail.com>
2022-11-14 16:21:22 -0500

    elipsis tut start

--- Elipsis_in_R/temp.R
+++ Elipsis_in_R/temp.R
@@ -34,3 +34,58 @@ my_ellipsis_function <- function(...) {
 }
 
 my_ellipsis_function(some_number = 123, some_string = 'abc')
+
+
+
+my_vec <- c(1,2,3,4,5,6,7,8,9,10)
+
+test_function <- function(vector, ...){
+
+    extra_arguments <- list(...)
+
+    for (i in 1:length(extra_arguments)){
+        assign(x = names(extra_arguments)[i], value = extra_arguments[[i]])
+    }
+
+    ls()
+
+    # for (i in vector){
+    #     print(i)
+    # }
+
+    print(extra_arguments)
+}
+
+
+
+
+
+
+new_function <- function(...){
+
+    for (arg in list(...)){
+
+        square <- sqrt(arg)
+        print(square)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
