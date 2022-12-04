efae400a80b36a2cfca18bdbb2e45750a5b473bf
Oscar Wright <oscar-wright@mail.com>
2022-12-03 20:52:41 -0500

    testing env file

---
+++ Glassdoor/API_test.py
@@ -0,0 +1,12 @@
+
+# On terminal...
+# pip install glassdoor
+
+# import requests
+
+# response = requests.get(
+#     "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=120&t.k=fz6JLNDfgVs&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0")
+
+from linkedin_api import Linkedin
+
+api = Linkedin("oscar-wright@mail.com", )
